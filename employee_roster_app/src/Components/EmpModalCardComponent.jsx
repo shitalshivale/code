38d55a5a2dbe7cbb18cf6b8fb2b2390c999136f4
shitalshivale <shitalshivale18@gmail.com>
2025-01-10 23:30:37 +0530

    feat: modal style fixed

--- employee_roster_app/src/Components/EmpModalCardComponent.jsx
+++ employee_roster_app/src/Components/EmpModalCardComponent.jsx
@@ -26,22 +26,29 @@ const EmpModalCardComponent = ({employee,onClose,isOpen}) =>{
     return(
         <div className="modal-overlay" onClose={onClose}>
             <div className="modal-content" ref={modalRef}>
-                <span className="close-icon" onClick={onClose}>
-                   +
-                </span>
-                <h2>
-                <img
-                    src={"/" + employee.empPhoto}
-                    alt=''
-                    height="30"
-                    width="30" 
-                />
-                    {employee.empName} {employee.empLastName}
-                </h2>
-                <p>Job Title:{employee.jobTitle}</p>
-                <p>Job Description: {employee.jobDescription}</p>
-                <p>Age: {employee.empAge}</p>
-                <p>Date of Joining: {employee.dateOfJoining}</p>
+                <span className="close-icon" onClick={onClose}>+</span>
+                <div className="modal-columns"> 
+                    <div className="column"> 
+                        <h2> 
+                            <img 
+                                src={"/" + employee.empPhoto} 
+                                alt='' 
+                                height="30" 
+                                width="30" 
+                            /> 
+                           
+                        </h2> 
+                        <p>Job Title: {employee.jobTitle}</p> 
+                        <p>Age: {employee.empAge}</p> 
+                        <p>Date of Joining: {employee.dateOfJoining}</p> 
+                    </div> 
+                    <div className="column"> 
+                        <p> {employee.empName} {employee.empLastName} </p> 
+                        <hr/>
+                        <p>Job Description: {employee.jobDescription}</p> 
+                        
+                    </div> 
+                </div>
             </div>
         </div>
     );
